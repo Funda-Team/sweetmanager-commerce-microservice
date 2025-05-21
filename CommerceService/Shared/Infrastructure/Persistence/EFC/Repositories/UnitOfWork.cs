e61d46e9fc837397dc18130b3ec5f9d0adbf6e24
Giacomo202210029 <u202210029@upc.edu.pe>
2025-05-21 00:06:58 -0500

    feat: Added Commerce Context

---
+++ CommerceService/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using IamService.Shared.Domain.Repositories;
+
+namespace CommerceService.Shared.Infrastructure.Persistence.EFC.Repositories
+{
+    public class UnitOfWork(CommerceContext context) : IUnitOfWork
+    {
+        public async Task CommitAsync() => await context.SaveChangesAsync();
+    }
+}
